3e323ba867594cc04f3a1c7e02428d48e8394321
ArtourKowalski <108827649+ArtourKowalski@users.noreply.github.com>
2022-07-11 13:15:32 +0200

    Update: README.md notebooks

--- notebooks/connectingSnowflake.py
+++ notebooks/connectingSnowflake.py
@@ -57,3 +57,62 @@ diabetesPositiveDF.write.saveAsTable("POSITIVE_DIABETES")
 # MAGIC %sql
 # MAGIC SELECT * FROM POSITIVE_DIABETES
 # MAGIC LIMIT 5;
+
+# COMMAND ----------
+
+user = "ArturK"
+password = "DemoPassword1!"
+
+options = { 
+"sfUrl": "ey57100.west-europe.azure.snowflakecomputing.com/",
+"sfUser": user,
+"sfPassword": password,
+"sfDatabase": "DATABRICKS",
+"sfSchema": "SALARIES",
+"sfWarehouse": "COMPUTE_WH"
+}
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Let's assume that we have multiple CSV files stored in Databricks and want to load it into one, master CSV file:
+
+# COMMAND ----------
+
+# load all partitions files into salaries DataFrame from DBFS
+
+file_list = []
+path = '/FileStore/tables/salaries/'
+files = dbutils.fs.ls(path)
+
+# loop through each file and add it into file_list if name starts with "partition"
+for file in files:
+    if(file.name.startswith("partition")):
+        file_list.append(path + file.name)
+
+salariesDF = spark.read.load(path=file_list, format="csv", sep=",", inferSchema="true", header="true")
+
+# COMMAND ----------
+
+# sort created DataFrame by id column
+salariesDF = salariesDF.sort(salariesDF.id.asc())
+salariesDF.display()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Right now, id values start from 0 and should from 1, let's change it using Python:
+
+# COMMAND ----------
+
+salariesDF = salariesDF.withColumn("id", salariesDF.id+1)
+salariesDF.display()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Write data to Snowflake:
+
+# COMMAND ----------
+
+salariesDF.write.format("snowflake").options(**options).option("dbtable", "ds_salaries").mode("append").save()
